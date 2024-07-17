f9eae0238190c4a9f4fee8ffac692e6188d7eb08
Lucas Tadeu Abreu de Melo <lucas.melo@appiancapitalbrazil.com>
2024-07-17 20:37:06 -0300

    conectando projeto no banco de dados

---
+++ src/test/java/com/backend/gestaoestoque/GestaoestoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.gestaoestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoestoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
